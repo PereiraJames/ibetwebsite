237a32a29037f5ae3709b6f4e1b238eb15da5e2a
PereiraJames <jamespereira_5@hotmail.com>
2025-02-14 17:56:56 +0800

    site name addition

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from "dotenv";
 
-
-dotenv.config()
+dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
     port: process.env.NETWORK_PORT, // Change to the port you prefer
+    allowedHosts: [
+      process.env.SITE_NAME, // Add the blocked host here
+    ],
   },
 });
